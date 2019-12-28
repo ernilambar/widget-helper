5fd5e65bda2763a643e1c0ebba6e4ec01ce4361b
Nilambar Sharma <nilambar@outlook.com>
2019-12-28 12:55:31 +0545

    Add class

---
+++ src/WidgetHelper.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Widget helper class
+ *
+ * @package ErNilambar
+ * @subpackage WidgetHelper
+ * @since 1.0.0
+ */
+
+namespace ErNilambar\WidgetHelper;
+
+class WH extends WP_Widget {
+
+}
