8c80c37ab7f87c1d4c803f3ac311df455f326da1
Nilambar Sharma <nilambar@outlook.com>
2019-12-28 19:16:02 +0545

    Testing

---
+++ src/greetings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Widget helper class
+ *
+ * @package ErNilambar
+ * @subpackage WidgetHelper
+ * @since 1.0.0
+ */
+
+namespace HelloWorld;
+
+class Greetings {
+  public static function sayHelloWorld() {
+    return 'Hello World';
+  }
+}
