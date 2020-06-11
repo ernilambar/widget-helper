229389a751804ab1621fbd7bd12aaff46b4f17fb
Nilambar Sharma <nilambar@outlook.com>
2020-06-11 13:19:27 +0545

    Add assets class

---
+++ src/Assets.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Assets
+ *
+ * @author    Nilambar Sharma <nilambar@outlook.com>
+ * @copyright 2020 Nilambar Sharma
+ *
+ * @package WidgetHelper
+ */
+
+namespace Nilambar\WidgetHelper;
+
+class Assets {
+
+}
