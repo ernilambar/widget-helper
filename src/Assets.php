f31b5bda2ec3881eea6e8a236baa44413dafe942
Nilambar Sharma <nilambar@outlook.com>
2020-06-11 15:07:09 +0545

    Fix issues

--- src/Assets.php
+++ src/Assets.php
@@ -10,39 +10,62 @@
 
 namespace Nilambar\WidgetHelper;
 
+/**
+ * Assets class.
+ *
+ * @since 1.0.0
+ */
 class Assets {
 
-	// Hold the class instance.
+	/**
+	 * An instances.
+	 *
+	 * @static
+	 * @access private
+	 * @since 1.0.1
+	 * @var array
+	 */
 	private static $instance = null;
 
+	/**
+	 * Load assets.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $type Type.
+	 */
 	public static function load_assets( $type = 'parent-theme' ) {
-		return self::get_instance()->load_all_assets( $type );
+		self::get_instance()->load_all_assets( $type );
 	}
 
-	public static function get_instance() {
-		if (self::$instance == null)
-		{
-			self::$instance = new Assets();
-		}
-
-		return self::$instance;
-	}
-
+	/**
+	 * Load all assets.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $type Type.
+	 */
 	public function load_all_assets( $type ) {
 		$base_url = $this->get_base_url( $type );
 
 		$asset_url = $base_url . '/vendor/ernilambar/widget-helper';
-
-		nspre( $asset_url );
 
 		wp_enqueue_style( 'wp-color-picker' );
 		wp_enqueue_script( 'wp-color-picker' );
 		wp_enqueue_media();
 
 		wp_enqueue_style( 'widget-helper', $asset_url . '/public/css/widgets.css', array(), '1.0.0' );
-		wp_enqueue_script( 'widget-helper', $asset_url . '/public/js/widgets.js', array('jquery', 'wp-color-picker'), '1.0.0' );
+		wp_enqueue_script( 'widget-helper', $asset_url . '/public/js/widgets.js', array( 'jquery', 'wp-color-picker' ), '1.0.0', true );
 	}
 
+	/**
+	 * Get base URL.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $type Type.
+	 * @return string Base URL.
+	 */
 	private function get_base_url( $type ) {
 		$url = '';
 
@@ -65,4 +88,20 @@ class Assets {
 
 		return $url;
 	}
+
+	/**
+	 * Get class instance.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return Assets Class instance.
+	 */
+	public static function get_instance() {
+		if ( null === self::$instance ) {
+			self::$instance = new Assets();
+		}
+
+		return self::$instance;
+	}
+
 }
